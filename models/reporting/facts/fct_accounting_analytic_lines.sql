5af900c3c9f699abb2d10f098ad2c29390bffe39
mpinto@bluwalk.com <mpinto@bluwalk.com>
2024-07-15 16:35:29 +0000

    fix on revenue and direct costs

--- models/reporting/facts/fct_accounting_analytic_lines.sql
+++ models/reporting/facts/fct_accounting_analytic_lines.sql
@@ -1,19 +1,26 @@
 SELECT
-    id,
-    date,
-    name,
-    amount,
-    account_id,
+    a.id,
+    a.date,
+    a.name,
+    a.amount,
+    a.account_id,
     b.analytic_account_type,
     b.analytic_account_name,
     b.analytic_account_owner_contact_id,
-    partner_id,
+    a.partner_id,
     a.product_id,
     c.product_code,
     a.rental_contract_id,
-    move_id,
-    payment_cycle,
+    a.vehicle_damage_id,
+    IFNULL(IFNULL(d.vehicle_license_plate, e.vehicle_license_plate), f.vehicle_license_plate) AS vehicle_license_plate,
+    IFNULL(IFNULL(d.vehicle_deal_name, e.vehicle_deal_name), f.vehicle_deal_name) AS vehicle_deal_name,
+    a.move_id,
+    a.payment_cycle
 FROM {{ ref('stg_odoo__account_analytic_lines') }} a
-left join {{ ref('dim_accounting_analytic_accounts') }} b ON a.account_id = b.analytic_account_id
+LEFT JOIN {{ ref('dim_accounting_analytic_accounts') }} b ON a.account_id = b.analytic_account_id
 LEFT JOIN {{ ref('dim_products') }} c ON a.product_id = c.product_id
-ORDER BY date DESC
+-- Vehicle Attribution
+LEFT JOIN {{ ref('fct_rental_contracts') }} d ON a.rental_contract_id = d.rental_contract_id
+LEFT JOIN {{ ref('fct_vehicle_damages') }} e ON a.vehicle_damage_id = e.id
+LEFT JOIN {{ ref('fct_vehicle_insurance_claims') }} f ON a.insurance_claim_id = f.id
+ORDER BY a.date DESC
---
+++ models/reporting/facts/fct_vehicle_insurance_claims.sql
@@ -0,0 +1,4 @@
+SELECT
+    a.*, b.vehicle_license_plate, b.vehicle_deal_name
+FROM {{ ref('stg_odoo__fleet_vehicle_insurance_claims') }} a
+LEFT JOIN {{ ref('dim_vehicles') }} b ON a.vehicle_id = b.vehicle_id
